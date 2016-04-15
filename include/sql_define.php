d1d6a54bdd2473b6173b3cd889e4bd70a93692c6
cddwx <cddwx525@163.com>
2016-04-15 23:46:19 +0800

    Update sql_define.php

--- include/sql_define.php
+++ include/sql_define.php
@@ -1,11 +1,11 @@
 <?php
-define('HOST', 'sql305.byethost9.com');
-define('USER', 'b9_16820414');
-define('PASSWORD', '2qD,wzL');
+define('HOST', 'xxxx');
+define('USER', 'xxxx');
+define('PASSWORD', 'xxxx');
 
-define('DB', 'b9_16820414_note');
+define('DB', 'xxxx');
 define('TABLE', 'article');
 
-define('ADMIN', 'fjc');
-define('ADMIN_PASS', 'qz1GWanC3');
+define('ADMIN', 'xxxx');
+define('ADMIN_PASS', 'xxxx');
 ?>
